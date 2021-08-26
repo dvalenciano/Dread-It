c18887907c2adb27a460340b905b4f574b5532fc
Brandon Hernandez <brandoncinthia0621@gmail.com>
2021-08-26 15:10:40 -0600

    updated async on controllers

--- controllers/TopicController.js
+++ controllers/TopicController.js
@@ -1,38 +1,31 @@
-const post = require("../models/post")
+const post = require('../models/post')
 
-const getBoxing = (req, res)=>{
+const getBoxing = async (req, res) => {
   const boxing = await BoxingArticle.find()
   res.json(boxing)
 }
 
-const getSoccer = (req, res)=>{
+const getSoccer = async (req, res) => {
   const soccer = await SoccerArticle.find()
   res.json(soccer)
 }
 
-const getFootball = (req, res)=>{
+const getFootball = async (req, res) => {
   const football = await FootballArticle.find()
   res.json(football)
 }
 
-const getArticleBoxing = (req, res)=>{
-  res.send({
-    
-  })
+const getArticleBoxing = (req, res) => {
+  res.send({})
 }
 
-const getArticleFootball = (req, res)=>{
-  res.send({
-    
-  })
+const getArticleFootball = (req, res) => {
+  res.send({})
 }
 
-const getArticleSoccer = (req, res)=>{
-  res.send({
-    
-  })
+const getArticleSoccer = (req, res) => {
+  res.send({})
 }
-
 
 module.exports = {
   getBoxing,
