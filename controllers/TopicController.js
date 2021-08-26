98c399ce5ba92396a869293e91969c1f2bcb2749
Derek <dvalenciano1986@gmail.com>
2021-08-26 10:58:19 -0500

    created controllers

---
+++ controllers/TopicController.js
@@ -0,0 +1,25 @@
+const getBoxing = (req, res)=>{
+  res.send({
+
+  })
+}
+
+const getFutbol = (req, res)=>{
+  res.send({
+    
+  })
+}
+
+const getFootball = (req, res)=>{
+  res.send({
+    
+  })
+}
+
+
+
+module.exports = {
+  getBoxing,
+  getFootball,
+  getFutbol
+}
