0db5bdfa73bff231cf6a9c831738719b32e48618
Sam Bassong <sam.bassong@gmail.com>
2021-08-26 16:41:24 -0700

    refactoring routes

--- models/index.js
+++ models/index.js
@@ -1,13 +1,9 @@
 const mongoose = require('mongoose')
 const PostSchema = require('./post')
 
-const SoccerArticle = mongoose.model('soccerarticles', PostSchema)
-const FootballArticle = mongoose.model('footballarticles', PostSchema)
-const BoxingArticle = mongoose.model('boxingarticles', PostSchema)
+const Post = mongoose.model('posts', PostSchema)
 
 module.exports = {
-  SoccerArticle,
-  FootballArticle,
-  BoxingArticle
+  Post
 }
 //update
