68a6ec69c87fa410de57485c528f22d8335b77a3
Derek <dvalenciano1986@gmail.com>
2021-08-26 16:14:43 -0500

    accepted conflicts

--- controllers/TopicController.js
+++ controllers/TopicController.js
@@ -1,28 +1,16 @@
 const post = require('../models/post')
 
-<<<<<<< HEAD
-const getBoxing = async (req, res)=>{
-=======
 const getBoxing = async (req, res) => {
->>>>>>> 1c9f7c136254eacbb470daaea7663370eaee817c
   const boxing = await BoxingArticle.find()
   res.json(boxing)
 }
 
-<<<<<<< HEAD
-const getSoccer = async (req, res)=>{
-=======
 const getSoccer = async (req, res) => {
->>>>>>> 1c9f7c136254eacbb470daaea7663370eaee817c
   const soccer = await SoccerArticle.find()
   res.json(soccer)
 }
 
-<<<<<<< HEAD
-const getFootball = async (req, res)=>{
-=======
 const getFootball = async (req, res) => {
->>>>>>> 1c9f7c136254eacbb470daaea7663370eaee817c
   const football = await FootballArticle.find()
   res.json(football)
 }
