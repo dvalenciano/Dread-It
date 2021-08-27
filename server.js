93251ba67e66272555441bd416b1b2d8a5a05854
Derek <dvalenciano1986@gmail.com>
2021-08-27 11:20:32 -0500

    finished

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -18,42 +19,42 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(logger('dev'))
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-app.get('/', (request, response) => {
-  response.send({ msg: 'Server Running' })
-})
+// app.get('/', (request, response) => {
+//   response.send({ msg: 'Server Running' })
+// })
 
-//postArticles
-app.post('/articles', async (req, res) => {
-  try {
-    const articles = await new Post(req.body)
-    await articles.save()
-    return res.status(200).send(articles)
-  } catch (error) {
-    console.log(error)
-  }
-})
+// //postArticles
+// app.post('/articles', async (req, res) => {
+//   try {
+//     const articles = await new Post(req.body)
+//     await articles.save()
+//     return res.status(200).send(articles)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// })
 
-//getArticles
-app.get('/articles', async (req, res) => {
-  try {
-    const articles = await Post.find()
-    // console.log(articles)
-    return res.json(articles)
-  } catch (error) {
-    console.log(error)
-  }
-})
+// //getArticles
+// app.get('/articles', async (req, res) => {
+//   try {
+//     const articles = await Post.find()
+//     // console.log(articles)
+//     return res.json(articles)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// })
 
-//getArticlesCategories
-app.get('/articles/:category', async (req, res) => {
-  try {
-    const articles = await Post.find({category: {$eq: req.params.category}})
-    // console.log(articles)
-    return res.json(articles)
-  } catch (error) {
-    console.log(error)
-  }
-})
+// //getArticlesCategories
+// app.get('/articles/:category', async (req, res) => {
+//   try {
+//     const articles = await Post.find({category: {$eq: req.params.category}})
+//     // console.log(articles)
+//     return res.json(articles)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// })
 
 // getComments
 // app.get('/articles/:category/comments', async (req, res) => {
@@ -67,6 +68,14 @@ app.get('/articles/:category', async (req, res) => {
 //   }
 // })
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req,res)=>{
+    res.sendfile(path.join(`${__dirname}/client/build/index.html`))
+  })
+}
 
 // End Your Code Here
 app.listen(PORT, () => console.log(`Server Listening on port: ${PORT}`))
+
+
